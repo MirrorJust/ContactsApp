070d080b035a6f3c9aa799afecff73a2bc8fdd20
MirrorJust <51231330+MirrorJust@users.noreply.github.com>
2021-03-25 17:29:17 +0500

    ScrollView change FlatList

--- ContactsApp/App.js
+++ ContactsApp/App.js
@@ -2,24 +2,39 @@ import React from 'react';
 import {
   StyleSheet,
   ScrollView,
+  FlatList,
 } from 'react-native';
 import {UserCard} from './src/components/UserCard.js';
 import {userArray} from './src/userData.js'
 
 const App = () => {
+  const renderItem = ({item}) => (
+    <UserCard 
+    name = {item.userName} 
+    phone = {item.userPhone} 
+    userpictures = {item.userPic}
+    userAdress = {item.addressDescription}
+    userMail = {item.contactDescription}
+    />
+  )
 
   return (
 
-    <ScrollView style = {styles.scrollStyle} contentContainerStyle = {styles.scrollContainer}>
-      {userArray.map(item=> (
-      <UserCard 
-      name = {item.userName} 
-      phone = {item.userPhone} 
-      userpictures = {item.userPic}
-      userAdress = {item.addressDescription}
-      userMail = {item.contactDescription}
-      />))}
-    </ScrollView>
+    <FlatList 
+      data={userArray}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      />
+    // <ScrollView style = {styles.scrollStyle} contentContainerStyle = {styles.scrollContainer}>
+    //   {userArray.map(item=> (
+    //   <UserCard 
+    //   name = {item.userName} 
+    //   phone = {item.userPhone} 
+    //   userpictures = {item.userPic}
+    //   userAdress = {item.addressDescription}
+    //   userMail = {item.contactDescription}
+    //   />))}
+    // </ScrollView>
     
   );
 };
